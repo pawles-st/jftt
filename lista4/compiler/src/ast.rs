daa8d6cc28cdafef413c611a41ae37e74bebf5d1
pawles-st <268419@gmail.com>
2023-12-27 19:55:27 +0100

    Create AST for compiler

---
+++ lista4/compiler/src/ast.rs
@@ -0,0 +1,131 @@
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct ProgramAll {
+    procedures: Procedures,
+    main: Main,
+}
+
+impl ProgramAll {
+    pub fn new(p: Procedures, m: Main) -> Self {
+        return Self{procedures: p, main: m};
+    }
+}
+
+pub type Procedures = Vec<Procedure>;
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Procedure {
+    proc_head: ProcHead,
+    declarations: Declarations,
+    commands: Commands,
+}
+
+impl Procedure {
+    pub fn new(ph: ProcHead, d: Declarations, c: Commands) -> Self {
+        return Self{proc_head: ph, declarations: d, commands: c};
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Main {
+    declarations: Declarations,
+    commands: Commands,
+}
+
+impl Main {
+    pub fn new(d: Declarations, c: Commands) -> Self {
+        return Self{declarations: d, commands: c};
+    }
+}
+
+pub type Commands = Vec<Command>;
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Command {
+    Assignment(Identifier, Expression),
+    IfElse(Condition, Commands, Commands),
+    If(Condition, Commands),
+    While(Condition, Commands),
+    Repeat(Commands, Condition),
+    ProcedureCall(ProcCall),
+    Read(Identifier),
+    Write(Value),
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct ProcHead {
+    name: Pidentifier,
+    args_decl: ArgumentDeclarations,
+}
+
+impl ProcHead {
+    pub fn new(p: Pidentifier, a: ArgumentDeclarations) -> Self {
+        return Self{name: p, args_decl: a};
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct ProcCall {
+    name: Pidentifier,
+    args: Arguments,
+}
+
+impl ProcCall {
+    pub fn new(p: Pidentifier, a: Arguments) -> Self {
+        return Self{name: p, args: a};
+    }
+}
+
+pub type Declarations = Vec<Declaration>;
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Declaration {
+    Var(Pidentifier),
+    Arr(Pidentifier, Num),
+}
+
+pub type ArgumentDeclarations = Vec<ArgumentDeclaration>;
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub enum ArgumentDeclaration {
+    Var(Pidentifier),
+    Arr(Pidentifier),
+}
+
+pub type Arguments = Vec<Pidentifier>;
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Expression {
+    Val(Value),
+    Add(Value, Value),
+    Sub(Value, Value),
+    Mul(Value, Value),
+    Div(Value, Value),
+    Mod(Value, Value),
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Condition {
+    Equal(Value, Value),
+    NotEqual(Value, Value),
+    Greater(Value, Value),
+    Lesser(Value, Value),
+    GreaterOrEqual(Value, Value),
+    LesserOrEqual(Value, Value),
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Value {
+    Number(Num),
+    Id(Identifier),
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Identifier {
+    Pid(Pidentifier),
+    ArrNum(Pidentifier, Num),
+    ArrPid(Pidentifier, Pidentifier),
+}
+
+pub type Pidentifier = String;
+
+pub type Num = u64;
---
+++ lista4/compiler/src/main.rs
@@ -0,0 +1,59 @@
+use std::fs;
+use std::io;
+
+use lalrpop_util::lalrpop_mod;
+use grammar::ProgramAllParser;
+
+pub mod err;
+pub mod ast;
+lalrpop_mod!(pub grammar);
+
+fn main() -> io::Result<()> {
+    let program = fs::read_to_string("../example3.imp")?;
+    match ProgramAllParser::new().parse(&program) {
+        Ok(ast) => println!("parsing succeeded!\n{:?}", ast),
+        Err(e) => eprintln!("Error: {:?}", e),
+    }
+    //let test = fs::read_to_string("../bignumber.imp")?;
+    //let result = ProgramAllParser::new().parse(&test);
+    //println!("{:?}", result);
+    /*
+    let program1 = fs::read_to_string("../example1.imp")?;
+    let result1 = ProgramAllParser::new().parse(&program1);
+    println!("ex1: {:?}", result1);
+
+    let program2 = fs::read_to_string("../example2.imp")?;
+    let result2 = ProgramAllParser::new().parse(&program2);
+    println!("ex2: {:?}", result2);
+
+    let program3 = fs::read_to_string("../example3.imp")?;
+    let result3 = ProgramAllParser::new().parse(&program3);
+    println!("ex3: {:?}", result3);
+
+    let program4 = fs::read_to_string("../example4.imp")?;
+    let result4 = ProgramAllParser::new().parse(&program4);
+    println!("ex4: {:?}", result4);
+
+    let program5 = fs::read_to_string("../example5.imp")?;
+    let result5 = ProgramAllParser::new().parse(&program5);
+    println!("ex5: {:?}", result5);
+
+    let program6 = fs::read_to_string("../example6.imp")?;
+    let result6 = ProgramAllParser::new().parse(&program6);
+    println!("ex6: {:?}", result6);
+
+    let program7 = fs::read_to_string("../example7.imp")?;
+    let result7 = ProgramAllParser::new().parse(&program7);
+    println!("ex7: {:?}", result7);
+
+    let program8 = fs::read_to_string("../example8.imp")?;
+    let result8 = ProgramAllParser::new().parse(&program8);
+    println!("ex8: {:?}", result8);
+
+    let program9 = fs::read_to_string("../example9.imp")?;
+    let result9 = ProgramAllParser::new().parse(&program9);
+    println!("ex9: {:?}", result9);
+    */
+
+    Ok(())
+}
